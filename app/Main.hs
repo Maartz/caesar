eafa2d78e754618dab96137d7098835c49437ab2
William Martin <maartz@icloud.com>
2023-08-29 22:43:02 +0200

    add type and checking functions

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,30 @@ module Main (main) where
 
 import Lib
 
+type Alphabet = [Char]
+
+lowerAlphabet :: Alphabet
+lowerAlphabet = ['a'..'z']
+
+upperAlphabet :: Alphabet
+upperAlphabet = ['A'..'Z']
+
+digits :: Alphabet
+digits = ['0'..'9']
+
+isLower :: Char -> Bool
+isLower char = char `elem` lowerAlphabet
+
+isUpper :: Char -> Bool
+isUpper char = char `elem` upperAlphabet
+
+isDigit :: Char -> Bool
+isDigit digit = digit `elem` digits
+
+isMisc :: Char -> Bool
+isMisc char = char `notElem` lowerAlphabet ++ upperAlphabet ++ digits
+
 main :: IO ()
 main = someFunc
+
+
